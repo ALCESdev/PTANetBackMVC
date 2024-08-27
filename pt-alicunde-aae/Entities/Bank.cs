4a06c58f1f249d939f6b69df383e659a0e8b15c7
Alejandro Alcantara Escoda <alejandroalces@hotmail.com>
2024-08-27 18:57:32 +0200

    Added entity validations through dataannotations

--- pt-alicunde-aae/Entities/Bank.cs
+++ pt-alicunde-aae/Entities/Bank.cs
@@ -1,9 +1,18 @@
-﻿namespace pt_alicunde_aae.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pt_alicunde_aae.Entities;
 
 public class Bank
 {
     public int id { get; set; }
+
+    [Required(ErrorMessage = "BIC is required.")]
+    [StringLength(11, ErrorMessage = "BIC cannot be longer than 11 characters.")]
     public string bic { get; set; }
+
+    [Required(ErrorMessage = "Country is required.")]
     public string country { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
     public string name { get; set; }
 }
