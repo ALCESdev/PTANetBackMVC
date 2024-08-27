1ff31e72a00f741ce1481665564146b92270c085
Alejandro Alcantara Escoda <alejandroalces@hotmail.com>
2024-08-27 18:59:58 +0200

    Added more tsks to bankservice and its documentation

--- pt-alicunde-aae/Services/BankService.cs
+++ pt-alicunde-aae/Services/BankService.cs
@@ -1,11 +1,18 @@
-﻿using pt_alicunde_aae.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using pt_alicunde_aae.Data;
 using pt_alicunde_aae.Entities;
 
 public class BankService
 {
+    #region VARIABLES
+
     private readonly HttpClient _httpClient;
     private readonly AppDbContext _context;
     private readonly ILogger<BankService> _logger;
+
+    #endregion
+
+    #region CONSTRUCTOR
 
     public BankService(HttpClient httpClient, AppDbContext context, ILogger<BankService> logger)
     {
@@ -14,6 +21,14 @@ public class BankService
         _logger = logger;
     }
 
+    #endregion
+
+    #region METHODS
+
+    /// <summary>
+    /// Fetches and stores banks from example API.
+    /// </summary>
+    /// <returns>A boolean indicating whether the operation was successful or not.</returns>
     public async Task<bool> FetchAndStoreBanksAsync()
     {
         try
@@ -36,4 +51,60 @@ public class BankService
             return false;
         }
     }
+
+    /// <summary>
+    /// Gets all banks from the database.
+    /// </summary>
+    /// <returns>A list of all banks.</returns>
+    public async Task<List<Bank>> GetAllBanksAsync() => await _context.Bank.ToListAsync();
+
+    /// <summary>
+    /// Gets a specific bank by its id.
+    /// </summary>
+    /// <param name="id">The bank's id.</param>
+    /// <returns>The requested bank if found; otherwise, null.</returns>
+    public async Task<Bank?> GetBankByIdAsync(int id) => await _context.Bank.FindAsync(id);
+
+    /// <summary>
+    /// Updates an existing bank in the database.
+    /// </summary>
+    /// <param name="id">The bank's id to update.</param>
+    /// <param name="updatedBank">The updated bank data.</param>
+    /// <returns>A boolean indicating whether the update was successful.</returns>
+    public async Task<bool> UpdateBankAsync(int id, Bank updatedBank)
+    {
+        Bank? existingBank = await _context.Bank.FindAsync(id);
+        if (existingBank == null)
+        {
+            return false;
+        }
+
+        existingBank.bic = updatedBank.bic;
+        existingBank.country = updatedBank.country;
+        existingBank.name = updatedBank.name;
+
+        _context.Bank.Update(existingBank);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    /// <summary>
+    /// Deletes a specific bank from the database.
+    /// </summary>
+    /// <param name="id">The bank's id to delete.</param>
+    /// <returns>A boolean indicating whether the deletion was successful.</returns>
+    public async Task<bool> DeleteBankAsync(int id)
+    {
+        Bank? bank = await _context.Bank.FindAsync(id);
+        if (bank == null)
+        {
+            return false;
+        }
+
+        _context.Bank.Remove(bank);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    #endregion
 }
